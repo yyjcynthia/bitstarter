ae7280ecaaf0cfb89af0cb342a5981fb974ee3e5
Ubuntu <ubuntu@ip-172-31-31-114.us-west-2.compute.internal>
2013-07-12 10:37:28 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,12 +1,22 @@
-var express = require('express');
 
+var express = require('express');
+var fs = require ('fs');
+var buf = require('buffer');
+var buffer = new Buffer(30);
+var outfile = "./index.html";
+var outpu = fs.readFileSync(outfile, "utf-8");
+
+ buffer.write(outpu,"utf-8");
+
+var outstring = buffer.toString('utf-8');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send('outpu');
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
